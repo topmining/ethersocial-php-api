3900b51e6c53f1dc3114a1602575aaa777512e96
freewinter <37530682+freewinter@users.noreply.github.com>
2019-02-24 18:28:19 +0900

    #6. 트랜잭션 기록하기

--- api-config.php
+++ api-config.php
@@ -1,3 +1,8 @@
 <?php
 $api_host = "127.0.0.1";
 $api_port = 9545;
+
+$mysql_host = "127.0.0.1";
+$mysql_user = "esnapi";   // YOUR ID
+$mysql_pass = "1234";     // YOUR PASSWORD
+$mysql_database = "ethersocial";
