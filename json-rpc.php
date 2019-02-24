086e2bffb2594007588b34738f1e72df6e69b0f7
freewinter <37530682+freewinter@users.noreply.github.com>
2019-02-24 18:29:17 +0900

    #6. 트랜잭션 기록하기

--- json-rpc.php
+++ json-rpc.php
@@ -14,3 +14,28 @@ function getEtherRpc($host, $port, $method, $params, $id) {
 
 	return $ret;
 }
+
+function getEtherRpcMulti($host, $port, $list) {
+	$ch = curl_init();
+
+	curl_setopt($ch, CURLOPT_URL, $host);
+	curl_setopt($ch, CURLOPT_PORT, $port);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+	curl_setopt($ch, CURLOPT_POST, TRUE);
+
+  	$data = "[";
+	$n = 0;
+	foreach($list as $item) {
+		if($n > 0) $data .= ",";
+		$data .= '{"jsonrpc":"2.0","method":"'.$item['method'].'","params":['.$item['params'].'],"id":'.$item['id'].'}';
+		$n++;
+	}
+	$data .= "]";
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+
+	$ret = curl_exec($ch);
+	curl_close($ch);
+
+	return $ret;
+}
